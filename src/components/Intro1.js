284b342cbd1e71d12f9f217df7f6f0776b2cf8a0
ahmedaryf <ahmed.areef@hotmail.com>
2023-02-25 22:36:07 +0500

    added horizontal line

--- src/components/Intro1.js
+++ src/components/Intro1.js
@@ -1,14 +1,18 @@
 function Intro1(props) {
-    const color = {
-        color: props.textColor
+    const color1 = {
+        color: props.textColor,
+        fontWeight: 900,
+        letterSpacing: '4px',
+        textAlign: 'center'
     }
      return (
             <div className="blog-post-intro">
-                <h2 style={color}>I've become a React developer! {props.name}</h2>
+                <h2 style={color1}>I've become a React developer! {props.name}</h2>
                 <div>
                     <p>{props.text}</p>
                     <p className="link">Read more...</p>
                 </div>
+                <hr className="line"/>
             </div>
         );
     };
